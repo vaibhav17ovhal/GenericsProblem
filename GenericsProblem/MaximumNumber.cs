7c501a6ba327bf2f22984fc276ecc96f9c1d8d7e
Vaibhav <vaibhavovhal15@gmail.com>
2023-05-13 23:14:49 +0530

    [Vaibhav] Add:Find maximum between 3 integer

---
+++ GenericsProblem/MaximumNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProblem
+{
+    public class MaximumNumber
+    {
+        public static int MaxInt(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0 ||
+                firstNumber.CompareTo(secondNumber) >= 0 && firstNumber.CompareTo(thirdNumber) > 0 ||
+                firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) >= 0)
+            {
+                return firstNumber;
+            }
+
+            else if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0 ||
+                secondNumber.CompareTo(firstNumber) >= 0 && secondNumber.CompareTo(thirdNumber) > 0 ||
+                secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) >= 0)
+            {
+                return secondNumber;
+            }
+
+            else if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0 ||
+                thirdNumber.CompareTo(firstNumber) >= 0 && thirdNumber.CompareTo(secondNumber) > 0 ||
+                thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) >= 0)
+            {
+                return thirdNumber;
+            }
+            return 0;
+        }
+    }
+}
---
+++ GenericsProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericsProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics");
+
+            int firstNumber = 15;
+            int secondNumber = 25;
+            int thirdNumber = 5;
+
+            int result = MaximumNumber.MaxInt(firstNumber, secondNumber, thirdNumber);
+            Console.WriteLine($"{result} is the greatest number");
+        }
+    }
+}
