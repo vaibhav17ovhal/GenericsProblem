1393193895bb24c031f60ca6d3b127f7b630813f
Vaibhav <vaibhavovhal15@gmail.com>
2023-05-13 23:38:11 +0530

    [Vaibhav] Add:Max using generic class

--- GenericsProblem/GenericMax.cs
+++ GenericsProblem/GenericMax.cs
@@ -8,6 +8,14 @@ namespace GenericsProblem
 {
     public class GenericMax<T> where T : IComparable
     {
+        public T firstNumber3, secondNumber3, thirdNumber3;
+
+        public GenericMax(T firstNumber3, T secondNumber3, T thirdNumber3)
+        {
+            this.firstNumber3 = firstNumber3;
+            this.secondNumber3 = secondNumber3;
+            this.thirdNumber3 = thirdNumber3;
+        }
         public static T Max(T firstNumber3, T secondNumber3, T thirdNumber3)
         {
             if (firstNumber3.CompareTo(secondNumber3) > 0 && firstNumber3.CompareTo(thirdNumber3) > 0 ||
