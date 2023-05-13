8c7d38af308b78d66ad452ab783c55fb23297b71
vaibhav17ovhal <125484163+vaibhav17ovhal@users.noreply.github.com>
2023-05-13 23:20:41 +0530

    Update MaximumNumber.cs

--- GenericsProblem/MaximumNumber.cs
+++ GenericsProblem/MaximumNumber.cs
@@ -8,31 +8,6 @@ namespace GenericsProblem
 {
     public class MaximumNumber
     {
-        public static int MaxInt(int firstNumber, int secondNumber, int thirdNumber)
-        {
-            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0 ||
-                firstNumber.CompareTo(secondNumber) >= 0 && firstNumber.CompareTo(thirdNumber) > 0 ||
-                firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) >= 0)
-            {
-                return firstNumber;
-            }
-
-            else if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0 ||
-                secondNumber.CompareTo(firstNumber) >= 0 && secondNumber.CompareTo(thirdNumber) > 0 ||
-                secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) >= 0)
-            {
-                return secondNumber;
-            }
-
-            else if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0 ||
-                thirdNumber.CompareTo(firstNumber) >= 0 && thirdNumber.CompareTo(secondNumber) > 0 ||
-                thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) >= 0)
-            {
-                return thirdNumber;
-            }
-            return 0;
-        }
-
         public static double MaxDouble(double firstNumber1, double secondNumber1, double thirdNumber1)
         {
             if (firstNumber1.CompareTo(secondNumber1) > 0 && firstNumber1.CompareTo(thirdNumber1) > 0 ||
