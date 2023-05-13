faaf2d5b8680f24a76f45ee5c1ece4b46b05fa66
Vaibhav <vaibhavovhal15@gmail.com>
2023-05-13 23:19:22 +0530

    [Vaibhav] Add:Find maximum between 3 float

--- GenericsProblem/MaximumNumber.cs
+++ GenericsProblem/MaximumNumber.cs
@@ -32,5 +32,30 @@ namespace GenericsProblem
             }
             return 0;
         }
+
+        public static double MaxDouble(double firstNumber1, double secondNumber1, double thirdNumber1)
+        {
+            if (firstNumber1.CompareTo(secondNumber1) > 0 && firstNumber1.CompareTo(thirdNumber1) > 0 ||
+                firstNumber1.CompareTo(secondNumber1) >= 0 && firstNumber1.CompareTo(thirdNumber1) > 0 ||
+                firstNumber1.CompareTo(secondNumber1) > 0 && firstNumber1.CompareTo(thirdNumber1) >= 0)
+            {
+                return firstNumber1;
+            }
+
+            else if (secondNumber1.CompareTo(firstNumber1) > 0 && secondNumber1.CompareTo(thirdNumber1) > 0 ||
+                secondNumber1.CompareTo(firstNumber1) >= 0 && secondNumber1.CompareTo(thirdNumber1) > 0 ||
+                secondNumber1.CompareTo(firstNumber1) > 0 && secondNumber1.CompareTo(thirdNumber1) >= 0)
+            {
+                return secondNumber1;
+            }
+
+            else if (thirdNumber1.CompareTo(firstNumber1) > 0 && thirdNumber1.CompareTo(secondNumber1) > 0 ||
+                thirdNumber1.CompareTo(firstNumber1) >= 0 && thirdNumber1.CompareTo(secondNumber1) > 0 ||
+                thirdNumber1.CompareTo(firstNumber1) > 0 && thirdNumber1.CompareTo(secondNumber1) >= 0)
+            {
+                return thirdNumber1;
+            }
+            return 0;
+        }
     }
 }
--- GenericsProblem/Program.cs
+++ GenericsProblem/Program.cs
@@ -14,6 +14,13 @@ namespace GenericsProblem
 
             int result = MaximumNumber.MaxInt(firstNumber, secondNumber, thirdNumber);
             Console.WriteLine($"{result} is the greatest number");
+
+            double firstNumber1 = 80.88;
+            double secondNumber1 = 50.55;
+            double thirdNumber1 = 10.11;
+
+            double result1 = MaximumNumber.MaxDouble(firstNumber1, secondNumber1, thirdNumber1);
+            Console.WriteLine($"{result1} is the greatest float number");
         }
     }
 }
