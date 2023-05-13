51527295cc2d4e2570e8ea49392361bf90bad53c
vaibhav17ovhal <125484163+vaibhav17ovhal@users.noreply.github.com>
2023-05-13 23:26:50 +0530

    Update Program.cs

--- GenericsProblem/Program.cs
+++ GenericsProblem/Program.cs
@@ -7,21 +7,7 @@ namespace GenericsProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Generics");
-
-            int firstNumber = 15;
-            int secondNumber = 25;
-            int thirdNumber = 5;
-
-            int result = MaximumNumber.MaxInt(firstNumber, secondNumber, thirdNumber);
-            Console.WriteLine($"{result} is the greatest number");
-
-            double firstNumber1 = 80.88;
-            double secondNumber1 = 50.55;
-            double thirdNumber1 = 10.11;
-
-            double result1 = MaximumNumber.MaxDouble(firstNumber1, secondNumber1, thirdNumber1);
-            Console.WriteLine($"{result1} is the greatest float number");
-
+            
             string firstNumber2 = "Vaibhav";
             string secondNumber2 = "vai";
             string thirdNumber2 = "vaibhavovhal";
